dbfcca2212e6c0e95f2d998951e51c57db89001c
muriloeduardo <muriloeduardoooooo@gmail.com>
2023-10-06 10:46:40 -0300

    Change to components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import { WSProvider } from './WebSocket'
+import { WSProvider } from '../components/WebSocket'
 
 const inter = Inter({ subsets: ['latin'] })
 
